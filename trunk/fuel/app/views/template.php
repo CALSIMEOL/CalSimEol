f89f70e6aceced995043fb654b5c41a3021ca488
antoine <antoine@6652f8e0-5e79-48e0-b992-bca5c3caf1f3>
2015-01-19 22:17:43 +0000

    Correction du lien 'Accueil' dans le templace.

--- trunk/fuel/app/views/template.php
+++ trunk/fuel/app/views/template.php
@@ -48,7 +48,7 @@
                 
                 <div id="navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="<?php echo Uri::base() ?>">Accueil</a></li>
+                        <li class="active"><a href="<?php echo Uri::create('home/index') ?>">Accueil</a></li>
                         <li><a href="<?php echo Uri::create('place/siteChoice') ?>">Simulation</a></li>
                         <li><a href="<?php echo Uri::create('home/about') ?>">A propos</a></li>
                     </ul>
