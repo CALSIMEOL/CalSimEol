2530398b1495d21fda7d68525c7dd0af6cfb4a4c
cyprien <cyprien@6652f8e0-5e79-48e0-b992-bca5c3caf1f3>
2015-01-22 13:33:15 +0000

    ajout du dossier "simulation" et de la vue "result"

---
+++ trunk/fuel/app/views/simulation/result.php
@@ -0,0 +1,18 @@
+<!-- Results -->
+
+        <div class="row">
+            <div class="clearness col-sm-12">
+                
+                <div class="row">
+                    <div class="lead col-sm-3">
+                        <h1>Résultats</h1>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    
+                </div>
+                
+            </div>
+        </div>
+
