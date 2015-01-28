cf3dfc19935571e307daac6c91e63cc7ff8d6cef
antoine <antoine@6652f8e0-5e79-48e0-b992-bca5c3caf1f3>
2015-01-28 13:29:38 +0000

    Ajout de nouvelles informations sur la page de résultats.

--- fuel/app/views/simulation/result.php
+++ fuel/app/views/simulation/result.php
@@ -29,10 +29,10 @@
                     <div class="col-sm-6">
                        <table class="table table-responsive table-striped table-condensed">
                           <tr>
-                            <td><b>&nbsp; Puissance moyenne surfacique en sortie :</td><td> <?php echo $power_mean ?> </td><td>W/m<sup>2</sup></td>
+                            <td><b>&nbsp; Puissance moyenne surfacique en sortie :</td><td> XXXX </td><td>W/m<sup>2</sup></td>
                           </tr>
                           <tr>
-                            <td><b>&nbsp; Puissance moyenne en sortie :</td><td> XXXX </td><td>kW</td>
+                            <td><b>&nbsp; Puissance moyenne en sortie :</td><td> <?php echo $power_mean ?> </td><td>kW</td>
                           </tr>
                           <tr>
                             <td><b>&nbsp; Production totale annuelle :</td><td> <?php echo $production ?> </td><td>kWh/an</td>
@@ -58,37 +58,9 @@
                                                         <th>Weibull à l'altitude du site [%]</th>
                                                         <th>Weibull à la hauteur du moyeu [%]</th>
                                                 </tr>
-                                                <tr><td>0</td><td></td><td></td></tr>
-                                                <tr><td>1</td><td></td><td></td></tr>
-                                                <tr><td>2</td><td></td><td></td></tr>
-                                                <tr><td>3</td><td></td><td></td></tr>
-                                                <tr><td>4</td><td></td><td></td></tr>
-                                                <tr><td>5</td><td></td><td></td></tr>
-                                                <tr><td>6</td><td></td><td></td></tr>
-                                                <tr><td>7</td><td></td><td></td></tr>
-                                                <tr><td>8</td><td></td><td></td></tr>
-                                                <tr><td>9</td><td></td><td></td></tr>
-                                                <tr><td>10</td><td></td><td></td></tr>
-                                                <tr><td>11</td><td></td><td></td></tr>
-                                                <tr><td>12</td><td></td><td></td></tr>
-                                                <tr><td>13</td><td></td><td></td></tr>
-                                                <tr><td>14</td><td></td><td></td></tr>
-                                                <tr><td>15</td><td></td><td></td></tr>
-                                                <tr><td>16</td><td></td><td></td></tr>
-                                                <tr><td>17</td><td></td><td></td></tr>
-                                                <tr><td>18</td><td></td><td></td></tr>
-                                                <tr><td>19</td><td></td><td></td></tr>
-                                                <tr><td>20</td><td></td><td></td></tr>
-                                                <tr><td>21</td><td></td><td></td></tr>
-                                                <tr><td>22</td><td></td><td></td></tr>
-                                                <tr><td>23</td><td></td><td></td></tr>
-                                                <tr><td>24</td><td></td><td></td></tr>
-                                                <tr><td>25</td><td></td><td></td></tr>
-                                                <tr><td>26</td><td></td><td></td></tr>
-                                                <tr><td>27</td><td></td><td></td></tr>
-                                                <tr><td>28</td><td></td><td></td></tr>
-                                                <tr><td>29</td><td></td><td></td></tr>
-                                                <tr><td>30</td><td></td><td></td></tr>
+<?php for ($i = 0; $i <= 30; $i++) : ?>
+                                                <tr><td><?php echo $i ?></td><td><?php echo $weibull_measure[$i] ?></td><td><?php echo $weibull_moyeu[$i] ?></td></tr>
+<?php endfor ?>
                                         </tbody></table>
                                  </div>
                             </div>
@@ -105,37 +77,9 @@
                                                     <th>Puissance [kW]</th>
                                                     <th>Cp</th>
                                             </tr>
-                                            <tr><td>0</td><td></td><td></td></tr>
-                                            <tr><td>1</td><td></td><td></td></tr>
-                                            <tr><td>2</td><td></td><td></td></tr>
-                                            <tr><td>3</td><td></td><td></td></tr>
-                                            <tr><td>4</td><td></td><td></td></tr>
-                                            <tr><td>5</td><td></td><td></td></tr>
-                                            <tr><td>6</td><td></td><td></td></tr>
-                                            <tr><td>7</td><td></td><td></td></tr>
-                                            <tr><td>8</td><td></td><td></td></tr>
-                                            <tr><td>9</td><td></td><td></td></tr>
-                                            <tr><td>10</td><td></td><td></td></tr>
-                                            <tr><td>11</td><td></td><td></td></tr>
-                                            <tr><td>12</td><td></td><td></td></tr>
-                                            <tr><td>13</td><td></td><td></td></tr>
-                                            <tr><td>14</td><td></td><td></td></tr>
-                                            <tr><td>15</td><td></td><td></td></tr>
-                                            <tr><td>16</td><td></td><td></td></tr>
-                                            <tr><td>17</td><td></td><td></td></tr>
-                                            <tr><td>18</td><td></td><td></td></tr>
-                                            <tr><td>19</td><td></td><td></td></tr>
-                                            <tr><td>20</td><td></td><td></td></tr>
-                                            <tr><td>21</td><td></td><td></td></tr>
-                                            <tr><td>22</td><td></td><td></td></tr>
-                                            <tr><td>23</td><td></td><td></td></tr>
-                                            <tr><td>24</td><td></td><td></td></tr>
-                                            <tr><td>25</td><td></td><td></td></tr>
-                                            <tr><td>26</td><td></td><td></td></tr>
-                                            <tr><td>27</td><td></td><td></td></tr>
-                                            <tr><td>28</td><td></td><td></td></tr>
-                                            <tr><td>29</td><td></td><td></td></tr>
-                                            <tr><td>30</td><td></td><td></td></tr>
+<?php for ($i = 0; $i <= 30; $i++) : ?>
+                                            <tr><td><?php echo $i ?></td><td><?php echo $turbine_power[$i] ?></td><td><?php echo $cp[$i] ?></td></tr>
+<?php endfor ?>
                                     </tbody></table>
                              </div>
                         </div>
@@ -153,37 +97,9 @@
                                                     <th>Vitesse stabilisée [m/s]</th>
                                                     <th>Puissance produite [kW]</th>
                                             </tr>
-                                            <tr><td>0</td><td></td></tr>
-                                            <tr><td>1</td><td></td></tr>
-                                            <tr><td>2</td><td></td></tr>
-                                            <tr><td>3</td><td></td></tr>
-                                            <tr><td>4</td><td></td></tr>
-                                            <tr><td>5</td><td></td></tr>
-                                            <tr><td>6</td><td></td></tr>
-                                            <tr><td>7</td><td></td></tr>
-                                            <tr><td>8</td><td></td></tr>
-                                            <tr><td>9</td><td></td></tr>
-                                            <tr><td>10</td><td></td></tr>
-                                            <tr><td>11</td><td></td></tr>
-                                            <tr><td>12</td><td></td></tr>
-                                            <tr><td>13</td><td></td></tr>
-                                            <tr><td>14</td><td></td></tr>
-                                            <tr><td>15</td><td></td></tr>
-                                            <tr><td>16</td><td></td></tr>
-                                            <tr><td>17</td><td></td></tr>
-                                            <tr><td>18</td><td></td></tr>
-                                            <tr><td>19</td><td></td></tr>
-                                            <tr><td>20</td><td></td></tr>
-                                            <tr><td>21</td><td></td></tr>
-                                            <tr><td>22</td><td></td></tr>
-                                            <tr><td>23</td><td></td></tr>
-                                            <tr><td>24</td><td></td></tr>
-                                            <tr><td>25</td><td></td></tr>
-                                            <tr><td>26</td><td></td></tr>
-                                            <tr><td>27</td><td></td></tr>
-                                            <tr><td>28</td><td></td></tr>
-                                            <tr><td>29</td><td></td></tr>
-                                            <tr><td>30</td><td></td></tr>
+<?php for ($i = 0; $i <= 30; $i++) : ?>
+                                            <tr><td><?php echo $i ?></td><td><?php echo $production_power[$i] ?></td></tr>
+<?php endfor ?>
                                     </tbody></table>
                                 </div>
                         </div>
@@ -201,8 +117,8 @@
                                                     <th>Densité de puissance en entrée avec limite de Betz [w/m<sup>2</sup>]</th>
                                                     <th>Densité de puissance en sortie [w/m<sup>2</sup>]</th>
                                             </tr>
-<?php for ($i = 0; $i < 301; $i++) : ?>
-                                            <tr><td><?php echo $i ?></td><td><?php echo $density_input[$i] ?></td><td></td><td><?php echo $density_output[$i] ?></td></tr>
+<?php for ($i = 0; $i <= 300; $i++) : ?>
+                                            <tr><td><?php echo $i ?></td><td><?php echo $density_input[$i] ?></td><td><?php echo $density_input_betz[$i] ?></td><td><?php echo $density_output[$i] ?></td></tr>
 <?php endfor ?>
                                     </tbody></table>
                             </div>
